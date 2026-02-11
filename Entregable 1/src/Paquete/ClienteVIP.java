7158faa96a7c686c3baf17e5a494d10ba071a5e9
Nash Rojas <nashjuan02@gmail.comclearcls>
2026-02-11 00:40:17 -0400

    Creacion ClienteVIP

---
+++ Entregable 1/src/Paquete/ClienteVIP.java
@@ -0,0 +1,16 @@
+package Paquete;
+
+public class ClienteVIP extends Cliente {
+    
+    private double porcentajeDescuento;
+
+    public ClienteVIP(int id, String nombre, double porcentajeDescuento){
+        super(id,nombre);
+        this.porcentajeDescuento = porcentajeDescuento;
+    }
+
+    @Override
+    public double calcularDescuento(double subtotal){
+        return subtotal * porcentajeDescuento / 100;
+    }
+}
