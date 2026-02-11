54f03d914843cf48caaca474fb6c4bc6fba84e94
Nash Rojas <nashjuan02@gmail.comclearcls>
2026-02-11 00:21:53 -0400

    creacion clase abstracta cliente

---
+++ Entregable 1/Paquete/Cliente.java
@@ -0,0 +1,21 @@
+
+public abstract class Cliente {
+    
+    private int id;
+    private String nombre;
+
+    public Cliente(int id, String nombre){
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public abstract double calcularDescuento(double subtotal);
+}
